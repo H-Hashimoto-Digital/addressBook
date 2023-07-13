9743eec8cc6324e65f54bca98dcd93f5195dc6ba
H-Hashimoto-Digital <hhashimoto@digital-corp.co.jp>
2023-07-13 13:54:27 +0900

    CSS呼び出しをコメントアウト（無効化）

--- php_training/mypage.php
+++ php_training/mypage.php
@@ -7,8 +7,8 @@
 <html>
 <head>
 <meta charset="utf-8" />
-<link rel="stylesheet" href="./css/common.css">
-<link rel="stylesheet" href="./css/mypage.css">
+<!--<link rel="stylesheet" href="./css/common.css">-->
+<!--<link rel="stylesheet" href="./css/mypage.css">-->
 <!-- BootstrapのCSS読み込み -->
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous"><!-- jQuery読み込み -->
 <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
